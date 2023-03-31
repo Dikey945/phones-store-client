614416ed491acd4b2c1ed51b63bd012639048ecf
Dima <ddikey1@gmail.com>
2023-03-31 16:44:35 +0300

    fixed UI bags(base rout spaces, return func in Back Button)

--- components/UI/BaseRoute.vue
+++ components/UI/BaseRoute.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex space-x-[8px] items-center">
     <nuxt-link to="/">
-      <font-awesome-icon class="cursor-pointer" :icon="['fas', 'house']" />
+      <font-awesome-icon class="cursor-pointer w-[24px] h-[24px]" :icon="['fas', 'house']" />
     </nuxt-link>
     <div class="flex space-x-[8px] items-center" v-for="link in route">
       <font-awesome-icon :icon="['fas', 'chevron-right']" />
-      <nuxt-link class="cursor-pointer" :to="link">{{ link.charAt(0).toUpperCase() + link.slice(1) }}</nuxt-link>
+      <nuxt-link class="cursor-pointer" :to="`/${link}`">{{ (link.charAt(0).toUpperCase() + link.slice(1)).split('-').join(' ') }}</nuxt-link>
     </div>
   </div>
 </template>
--- components/phonePage/BackButton.vue
+++ components/phonePage/BackButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center space-x-[4px]">
+  <div class="flex items-center space-x-[4px] cursor-pointer" @click="goBack">
     <svg fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
          viewBox="0 0 404.258 404.258" xml:space="preserve">
       <polygon points="289.927,18 265.927,0 114.331,202.129 265.927,404.258 289.927,386.258 151.831,202.129 "/>
@@ -21,7 +21,9 @@ export default {
       router.push({path: route})
     }
 
-    return {}
+    return {
+      goBack,
+    }
   }
 }
 </script>
